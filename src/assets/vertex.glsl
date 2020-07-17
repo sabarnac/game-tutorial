9c8a1446d0bd750ac383484e5e5251ff75d80280
Sabarna Chakravarty <sabarnac@gmail.com>
2020-07-17 15:13:18 +0530

    Part 2 Talk

--- src/assets/vertex.glsl
+++ src/assets/vertex.glsl
@@ -2,19 +2,21 @@
 
 // We'll receive the position of a single vertex here.
 layout(location = 0) in vec3 vertexPosition;
+layout(location = 1) in vec2 vertexUv;
+
+out vec2 fragmentUv;
 
 // We'll output the position of the current vertex to the fragment shader through here.
 // For fragments in between vertices, inside the primitive shape being drawn by the GPU
 //   (triangles in our case), it will use the distance of the vertices of that shape as
 //   weights for linearly interpolating what the value of this output should be for the
 //   fragment that is in-between those vertices and inside the shape.
-out vec3 fragmentPosition;
+uniform mat4 mvpMatrix;
 
 void main()
 {
   // Set the position of the vertex the same as the vertex position we retrieved.
-  gl_Position = vec4(vertexPosition, 1.0);
-  // Set the position of the fragment as the position of our vertex. The GPU will linearly
-  //   interpolate this value to determine what is the correct value for each fragment.
-  fragmentPosition = vertexPosition;
+  gl_Position = mvpMatrix * vec4(vertexPosition, 1.0);
+
+  fragmentUv = vertexUv;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,14 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 #include "include/shader.cpp"
+#include "include/object.cpp"
+#include "include/texture.cpp"
 
 using namespace glm;
 
@@ -78,27 +84,56 @@ int main(void)
 	glGenVertexArrays(1, &vertexArrayID);
 	glBindVertexArray(vertexArrayID);
 
-	// Define what the vertices of our triangle look like.
-	const GLfloat triangleVertices[] = {
-			-1, -1, 0,
-			1, -1, 0,
-			0, 1, 0};
-
-	// Get an instance of the shader manager and generate the shader program for the triangle.
+	// Get an instance of the shader manager and generate the shader program for the cube.
 	auto &shaderManager = ShaderManager::getInstance();
-	auto shaderDetails = shaderManager.createShaderProgram("TriangleShader", "assets/vertex.glsl", "assets/fragment.glsl");
+	auto shaderDetails = shaderManager.createShaderProgram("CubeShader", "assets/vertex.glsl", "assets/fragment.glsl");
 
-	// Generate a buffer object for storing our triangle vertices data into.
-	GLuint vertexBuffer;
-	glGenBuffers(1, &vertexBuffer);
-	// Bind the generated vertex buffer as an array buffer, telling the GPU it will store data as an array of elements.
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-	// Now that we're working under the context of the currently bound vertex buffer, copy the data of the triangle vertices,
-	//   providing the size and reference to the data, and how the data is being used.
-	// STATIC -> The data won't be changed.
-	// DRAW -> The data will be used for drawing/rendering.
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// Get an instance of the object manager and read and store the object details of the cube.
+	auto &objectManager = ObjectManager::getInstance();
+	auto cubeObjectDetails = objectManager.createObject("CubeObject", "assets/objects/cube.obj");
+
+	// Get an instance of the texture manager and read and store the surface image texture of the cube.
+	auto &textureManager = TextureManager::getInstance();
+	auto cubeTextureDetails = textureManager.create2dTexture("CubeTexture", "assets/textures/cube.bmp");
+
+	// Define a matrix that represents how much the object should be scaled by (the scaling matrix - scale across x-axis, y-axis, z-axis).
+	glm::mat4 scalingMatrix = glm::scale(glm::vec3(1.5, 1.5, 1.5));
+	// Define a matrix that represents how much the object should be rotated by (the rotation matrix - rotation across x-axis, y-axis, z-axis).
+	glm::mat4 rotationMatrix = glm::toMat4(glm::quat(glm::vec3(5, 10, 15)));
+	// Define a matrix that represents how much the object should be translated/moved by (the translation matrix - translate across x-axis, y-axis, z-axis).
+	glm::mat4 translationMatrix = glm::translate(glm::vec3(0.5, 1, 1.5));
+
+	// Create the overall model transformation matrix (or just model matrix) by multiplying the individual sub-transformations in the order of:
+	//   1. Scaling
+	//   2. Rotation
+	//   3. Translation
+	// In code, this multiplication is done in the reverse order, since the matrix is multiplied against the vertex, which will be at the right side of the operation.
+	// This would result in the right side of this order having precedence with being multiplied against the vertex to change its position.
+	// This will transform the vertex from a position relative to the model, to its position in the world (called world-space).
+	glm::mat4 modelMatrix = translationMatrix * rotationMatrix * scalingMatrix;
+
+	// Define a view matrix that defines the location of the camera and the direction it is pointed towards. This will transform the model from being relative to the world
+	//   to instead be relative to the camera (called camera-space or view-space).
+	glm::mat4 viewMatrix = glm::lookAt(glm::vec3(3, 3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+
+	// Define a perspective matrix projection of the 3D world (relative to the camera) onto the viewport. We define the field-of-view of the viewport,
+	//   the aspect ratio of the viewport, and the closest and farthest the screen can see till.
+	// This will transform the model from being relative to the camera, to instead be relative to the screen in a 3D homogenous space (coordinate range of 0 - 1 across all axes).
+	// Anything outside the homogenous space (not within the coordinate range of 0 - 1 at any axis) can be considered to be not visible on the screen.
+	// This 3D homogenous space relative to the screen is called clip-space. It aids the GPU in determining what can be drawn on the screen and what can be discard, since
+	//   anything outside the homogenous coordinate range will not be visible.
+	// This space also provides the GPU information to perform a perspective division of the model to correctly size it based on perspective and distance from the screen.
+	glm::mat4 projectionMatrix = glm::perspective(glm::radians(60.0), 4.0 / 3.0, 0.1, 100.0);
+
+	// // Alternative: Define an orthogonal (flat) projection of the 2D world (relative to the camera) onto the viewport. We define the left, right, top,
+	// //   bottom boundaries of the screen, and the closest and farthest the screen can see till.
+	// An orthogonal projection assumes a flat projection, meaning that if two objects of the same size are at different distances, they will appear the same,
+	//   size on screen, unlike perspective projection which would shrink the object that is further back.
+	// So an orthogonal projection can capture a 2D world, whereas a perspective projection that can capture a 3D world.
+	// glm::mat4 projectionMatrix = glm::ortho(-8.0, 8.0, -6.0, 6.0, 0.1, 100.0);
+
+	// Calculate the final model-view-projection matrix product to be sent to the GPU to be applied to each vertex of the object.
+	glm::mat4 mvpMatrix = projectionMatrix * viewMatrix * modelMatrix;
 
 	do
 	{
@@ -108,27 +143,52 @@ int main(void)
 		// Use our created triangle shader program.
 		glUseProgram(shaderDetails->getShaderId());
 
-		// Enable the first vertex attribute array, which will carry our triangle vertices information and loop through them.
+		// Activate the 0th texture unit, which can then read data from the cube texture.
+		glActiveTexture(GL_TEXTURE0);
+		// Bind the cube texture as a 2D texture to the 0th texture unit (which is now the immediate context for the texture operation).
+		glBindTexture(GL_TEXTURE_2D, cubeTextureDetails->getTextureId());
+		// Get the location ID of the uniform which represents the texture sampler variable defined in the shader.
+		auto cubeTextureUniformId = glGetUniformLocation(shaderDetails->getShaderId(), "cubeTexture");
+		// Assign the texture sampler variable in the shader a value of 0, so that it will read texture data from the 0th texture unit.
+		glUniform1i(cubeTextureUniformId, 0);
+
+		// Get the location ID of the uniform which represents the model-view-projection matrix variable defined in the shader.
+		auto mvpMatrixUniformId = glGetUniformLocation(shaderDetails->getShaderId(), "mvpMatrix");
+		// Assign the model-view-projection matrix variable in the shader the model-view-projection matrix result that we calculated.
+		// We flag that we are only passing 1 matrix to the GPU, and that it should not be transposed before sending.
+		glUniformMatrix4fv(mvpMatrixUniformId, 1, GL_FALSE, &mvpMatrix[0][0]);
+
+		// Enable the first vertex attribute array, which will carry our triangle vertices position information and loop through them.
 		glEnableVertexAttribArray(0);
-		// Bind the vertex buffer as an array buffer to the 0th vertex attribute array.
-		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-		// Define the attributes of the data being sent through the vertex attribute array buffer.
+		// Bind the vertex buffer as an array buffer to the first vertex attribute array (which is now the immediate context for the buffer operation).
+		glBindBuffer(GL_ARRAY_BUFFER, cubeObjectDetails->getVertexBufferId());
+		// Define the attributes of the data being sent through the first vertex attribute array buffer.
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-		// Draw the vertices such that every 3 consecutive vertices draw a triangle.
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Enable the second vertex attribute array, which will carry our triangle vertices UV coordinate information and loop through them.
+		glEnableVertexAttribArray(1);
+		// Bind the UV coordinates buffer as an array buffer to the second vertex attribute array (which is now the immediate context for the buffer operation).
+		glBindBuffer(GL_ARRAY_BUFFER, cubeObjectDetails->getUvBufferId());
+		// Define the attributes of the data being sent through the second vertex attribute array buffer.
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-		// Disable the vertex attribute array since it's done being used.
+		// Draw the vertices of the object such that every 3 consecutive vertices draws a triangle.
+		glDrawArrays(GL_TRIANGLES, 0, cubeObjectDetails->getBufferSize());
+
+		// Disable the vertex attribute arrays since they're done being used.
+		glDisableVertexAttribArray(1);
 		glDisableVertexAttribArray(0);
 
 		// Swap the window buffers.
 		glfwSwapBuffers(window);
 		// Poll for new control events.
 		glfwPollEvents();
-	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+	}
+	// Check if we need to exit
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 
-	// Delete the vertex buffer we created since it's no longer required.
-	glDeleteBuffers(1, &vertexBuffer);
+	// Delete the vertex buffer we created since it's no longer required
+	objectManager.destroyObject(cubeObjectDetails);
 
 	// Delete the vertex array object.
 	glDeleteVertexArrays(1, &vertexArrayID);
