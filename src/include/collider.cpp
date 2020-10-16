e92c405b00c6a7c8cb74bae3bd8fad850b7a2a17
Sabarna Chakravarty <sabarnac@gmail.com>
2020-10-17 03:17:22 +0530

    Add Headers To Colliders

--- src/include/collider.cpp
+++ src/include/collider.cpp
@@ -13,6 +13,10 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 class ColliderShape;
 class SphereColliderShape;
